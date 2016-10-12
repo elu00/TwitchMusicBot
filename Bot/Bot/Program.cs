7413099a9ab7a8c4bd654c9d41491b1216c7f50a
Ethan Lu <elu@davidsonacademy.unr.edu>
2016-10-12 16:46:52 -0700

    Added some basic file structure

---
+++ Bot/Bot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parse original configuration from config.xml
+
+            //Connect to twitch IRC channel
+
+            //Listen for commands
+
+            //Do commands
+        }
+    }
+}
