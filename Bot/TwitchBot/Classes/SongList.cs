4f4d9b75cb9f81cf8b4d18085b8ca4372cb6ae07
Ethan Lu <elu@davidsonacademy.unr.edu>
2016-11-27 19:56:12 -0800

    Fixed all major issues with the program, added a next song button, log and datagrid databinding now functional, reworking of data layout. Next commit will have scrollviewer of the log fixed and custom columns with song indexes implemented.

--- Bot/TwitchBot/Classes/SongList.cs
+++ Bot/TwitchBot/Classes/SongList.cs
@@ -1,47 +1,185 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using TwitchLib;
+using TwitchLib.Models.Client;
 
 namespace TwitchBot.Classes
 {
-    class SongList
+    //Stores all strings used for chat response/interactions for easy modification
+    struct msgs
     {
-        private ObservableCollection<SongRequest> list = new ObservableCollection<SongRequest>();
+        public const string About = "Open source bot made by Ethan Lu, browse my source and report issues at https://github.com/elu00/TwitchMusicBot";
+        public const string EmptyRequest = "Please specify the URL of your song with !request <url>";
+        public const string NotOnList = "You are not currently on the list";
+        public const string Ping = " -> ";
+        public const string CurrentSpot = "Your current spot in the list is ";
+        public const string ModsOnly = "Sorry, only the channel moderators can use this command!";
+        public const string Rules = "Song/loop must be under 90 seconds.Please try to only use youtube/soundcloud urls";
+        public const string Commands = "Available commands are: !request <song>, !spot, !drop, !list, !next, !currentsong, !change <song>";
+        public const string UnknownCommand = "Command not recognized";
+    }
+    class SongList : INotifyPropertyChanged
+    {
+        private TwitchClient client;
+        private List<string> mods = new List<string>();
+        private ObservableCollection<SongRequest> _list = new ObservableCollection<SongRequest>();
         public ObservableCollection<SongRequest> List
         {
             get
             {
-                return list;
+                return _list;
             }
             protected set
             {
-                list = value;
+                _list = value;
             }
+        }
+        private string _logContent;
+
+        public string LogContent
+        {
+            get { return _logContent; }
+            protected set { _logContent = value; }
+        }
+        public SongList()
+        {
+            _logContent = "List initialized \n";
+        }
+        public void initialize(string username, string oauth, string channel)
+        {
+            //Connect to twitch IRC channel
+            client = new TwitchClient(new ConnectionCredentials(username, oauth), channel, '!', '!', true);
+            log("Client created. Joining channel " + channel + " with username " + username, false);
+            //Event and Command Handlers
+            client.OnJoinedChannel += (sender, e) =>
+            {
+                log("Connected!", false);
+                client.SendMessage("Bot intialized");
+            };
+            client.OnModeratorsReceived += (sender, e) =>
+            {
+                log("Mods recieved", false);
+                mods.AddRange(e.Moderators);
+            };
+            client.OnChatCommandReceived += chatCommandReceived;
+
+            //Connect
+            client.Connect();
+            client.GetChannelModerators();
+        }
+        private void chatCommandReceived(object sender, TwitchLib.Events.Client.OnChatCommandReceivedArgs e)
+        {
+            Console.WriteLine("Command Recieved");
+            TwitchClient client = sender as TwitchClient;
+            string command = e.Command.Command;
+            string username = e.Command.ChatMessage.Username;
+            string args = e.Command.ArgumentsAsString;
+            switch (command.ToLower())
+            {
+                case "request":
+                    if (args == "")
+                    {
+                        log(msgs.EmptyRequest);
+                        break;
+                    }
+                    SongRequest request = new SongRequest(username, args);
+                    log(username + msgs.Ping + AddSong(request));
+                    break;
+                case "spot":
+                    int spot = GetSpot(username);
+                    if (spot == -1)
+                    {
+                        log(username + msgs.Ping + msgs.NotOnList);
+                        break;
+                    }
+                    log(username + msgs.Ping + msgs.CurrentSpot + spot.ToString());
+                    break;
+                case "drop":
+                    log(username + msgs.Ping + RemoveSong(username));
+                    break;
+                case "list":
+                    log(username + msgs.Ping + GetList());
+                    break;
+                case "commands":
+                    log(msgs.Commands);
+                    break;
+                case "rules":
+                    log(msgs.Rules);
+                    break;
+                case "change":
+                    log(username + msgs.Ping + ChangeRequest(username, args));
+                    break;
+                case "currentsong":
+                    log(GetCurrentSong());
+                    break;
+                case "about":
+                    log(msgs.About);
+                    break;
+                // Moderator restricted functions
+                case "next":
+                    if (mods.Contains(username))
+                    {
+                        log(Next());
+                        break;
+                    }
+                    else
+                    {
+                        log(username + msgs.Ping + msgs.ModsOnly);
+                        break;
+                    }
+                case "remove":
+                    if (mods.Contains(username))
+                    {
+                        log("(Mod Removal)" + args + msgs.Ping + RemoveSong(args));
+                        break;
+                    }
+                    else
+                    {
+                        log(username + msgs.Ping + msgs.ModsOnly);
+                        break;
+                    }
+                default:
+                    log(username + msgs.Ping + msgs.UnknownCommand);
+                    break;
+            }
+
         }
         //returns true if song is succesfully added, returns false when user is already in queue
         public string AddSong(SongRequest song)
         {
-            foreach (SongRequest item in list)
+            foreach (SongRequest item in _list)
             {
-                if(item.username==song.username)
+                if(item.Username==song.Username)
                 {
                     return "Error: you are already on the list. If you want to change your request, !change it.";
                 }
             }
-            list.Add(song);
-            return "Request successfully added. Your current spot in the list is " + (list.Count-1);
+
+            Application.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                _list.Add(song);
+            }));
+
+            return "Request successfully added. Your current spot in the list is " + (_list.Count-1);
         }
         public string RemoveSong(string username)
         {
-            foreach (SongRequest item in list)
+            foreach (SongRequest item in _list)
             {
-                if (item.username == username)
+                if (item.Username == username)
                 {
-                    list.RemoveAt(list.IndexOf(item));
+                    Application.Current.Dispatcher.Invoke((Action)(() =>
+                    {
+                        _list.RemoveAt(_list.IndexOf(item));
+                    }));
                     return "Your request has been removed from the list";
                 }
             }
@@ -51,40 +189,47 @@ namespace TwitchBot.Classes
         {
             /* Will be used to implement historical data/analysis to a corresponding text file
             StreamReader rec = new StreamReader("history.txt");
-            rec.WriteLine(list[0].url)
+            rec.WriteLine(_list[0].url)
             */
-            if (list[0] == null)
+            if (_list[0] == null)
             {
                 return "The list is currently empty";
             }
             else
             {
-                list.RemoveAt(0);
-                if(list[0] == null)
+                Application.Current.Dispatcher.Invoke((Action)(() =>
+                {
+                    _list.RemoveAt(0);
+                }));
+                if(_list.Count == 0)
                 {
                     return "That's the end of the list!";
                 }
-                return "Next song: " + list[0].summary;
+                return "Next song: " + _list[0].Summary;
             }
         }
         public string GetList()
         {
-            if(list.Count == 0)
+            if(_list.Count == 0)
             {
                 return "The list is currently empty";
             }
             string contents = "The first 5 people in the list are: (Currently Up) ";
             for (int i = 0; i < 4; i++)
             {
-                contents += i.ToString() + ". " + list[i].username + ", ";
+                if(_list.Count == i)
+                {
+                    break;
+                }
+                contents += i.ToString() + ". " + _list[i].Username + ", ";
 
             }
             return contents;
 
             /* Retired due to Twitch's 500 character limit per character. Likely will be used in web implementation though, so it's being kept around
             int count = -1;
-            string contents = "The list is ";
-            foreach (SongRequest song in list)
+            string contents = "The _list is ";
+            foreach (SongRequest song in _list)
             {
                 count++;
                 if(count==0)
@@ -101,25 +246,25 @@ namespace TwitchBot.Classes
         }
         public int GetSpot(string username)
         {
-			foreach (SongRequest user in list)
+			foreach (SongRequest user in _list)
 			{
-                //iterate through list and return index is user is inside
-                if(user.username == username)
+                //iterate through _list and return index is user is inside
+                if(user.Username == username)
                 {
-                    return list.IndexOf(user);
+                    return _list.IndexOf(user);
                 }
 			}
             return -1;
         }
         public string GetCurrentSong()
         {
-            if (list[0] == null)
+            if (_list[0] == null)
             {
                 return "The list is currently empty";
             }
             else
             {
-                return "The current song is "+ list[0].summary;
+                return "The current song is "+ _list[0].Summary;
             }
         }
 
@@ -130,18 +275,37 @@ namespace TwitchBot.Classes
                 return "Please specify the new URL of your request";
             }
             int index = -1;
-            foreach (SongRequest user in list)
+            foreach (SongRequest user in _list)
             {
-                //iterate through list and return index is user is inside
-                if (user.username == username)
+                //iterate through _list and return index is user is inside
+                if (user.Username == username)
                 {
-                    index = list.IndexOf(user);
+                    index = _list.IndexOf(user);
                 }
             }
-            list[index].url = args;
-            list[index].GenerateSummary();
+            Application.Current.Dispatcher.Invoke((Action)(() =>
+            {
+            _list[index].Url = args;
+            _list[index].GenerateSummary();
+            }));
             return "Your request has been sucessfully updated";
         }
-
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public void log(string msg, bool chat = true)
+        {
+            if (chat)
+            {
+                client.SendMessage(msg);
+            }
+            _logContent += (msg + "\n");
+            NotifyPropertyChanged("LogContent");
+        }
     }
 }
--- Bot/TwitchBot/Classes/SongRequest.cs
+++ Bot/TwitchBot/Classes/SongRequest.cs
@@ -1,30 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TwitchBot.Classes
 {
-    class SongRequest
+    class SongRequest : INotifyPropertyChanged
     {
-        public string username;
-        public string url;
-       // public string name;
-        public DateTime time;
-        public string summary;
+        private string _username;
+        private string _url;
+        private DateTime _time;
+        public string _summary;
 
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            protected set
+            {
+                _username = value;
+            }
+        }
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                if (_url == value)
+                {
+                    return;
+                }
+                else
+                {
+                    _url = value;
+                    NotifyPropertyChanged("Url");
+                }
+            }
+        }
+        public DateTime Time
+        {
+            get
+            {
+                return _time;
+            }
+            protected set
+            {
+                _time = value;
+            }
+        }
+        public string Summary
+        {
+            get
+            {
+                return _summary;
+            }
+            protected set
+            {
+                _summary = value;
+            }
+        }
+
+        // public string name;
         public SongRequest(string user, string url)
         {
-            username = user;
-            this.url = url;
-            time = DateTime.Now;
+            _username = user;
+            _url = url;
+            _time = DateTime.Now;
             //name = Youtube.Parse.Name(url);
             GenerateSummary();
         }
         public void GenerateSummary()
         {
-            summary = url + ", Requested By: " + username;
+            _summary = Url + ", Requested By: " + Username;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
